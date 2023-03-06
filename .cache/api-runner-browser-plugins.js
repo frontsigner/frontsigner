49f96c5ede69ce0a2380c2cad555aee496d78b02
Zabdiel Maestre <zabdielmaestre@gmail.com>
2023-03-06 17:10:45 -0500

    lazaro

--- .cache/api-runner-browser-plugins.js
+++ .cache/api-runner-browser-plugins.js
@@ -1,4 +1,7 @@
 module.exports = [{
+      plugin: require('../node_modules/gatsby-plugin-styled-components/gatsby-browser.js'),
+      options: {"plugins":[],"displayName":true,"fileName":true,"minify":true,"namespace":"","transpileTemplateLiterals":true,"topLevelImportPaths":[],"pure":false,"disableVendorPrefixes":false},
+    },{
       plugin: require('../node_modules/gatsby-plugin-google-gtag/gatsby-browser.js'),
       options: {"plugins":[],"trackingIds":["G-TQNSZP62DW"],"pluginConfig":{"head":false,"respectDNT":true}},
     }]
